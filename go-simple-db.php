edda102ac862be54b843817229e7687bd1e9d575
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:47:32 -0400

    Update go-simple-db.php

--- go-simple-db.php
+++ go-simple-db.php
@@ -5,6 +5,8 @@
  * Description:	Establish a connection to Amazon Simple DB
  * Author: 		GigaOM 
  * Author URI: 	http://gigaom.com/
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-simple-db.php';
